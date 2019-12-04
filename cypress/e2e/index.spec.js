9c4ccd619eb93fae0b5e037955d79ca41470694c
Angus Perkerson <angusp@angusp.com>
2019-12-03 22:09:21 -0800

    test: adds cypress testing framework
    closes #9

---
+++ cypress/e2e/index.spec.js
@@ -0,0 +1,11 @@
+describe('Home Page Test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.injectAxe();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+  });
+  it('Has no detectable a11y violations on load', () => {
+    cy.checkA11y();
+  });
+});
